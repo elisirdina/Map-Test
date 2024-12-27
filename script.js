7b1ae07f8fafc85e0cac0c4dbb89bd36cd235eb3
elisirdina <elisirdina.idr@gmail.com>
2024-12-27 15:26:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let map;
-
 function initMap() {
     map = new google.maps.Map(document.getElementById("map1"), {
-        center: { lat: -34.397, lng: 150.644 },
+        center: { lat: 28.70, lng: 77.10 },
         zoom: 8,
+        mapTypeId: "terrain"
+    });
+    new google.maps.Marker({
+        position: { lat: 28.70, lng: 77.10 },
+        map: map,
+        label: "A",
+        title: "New Delhi",
+        draggeable: false,
+        animation: google.maps.Animation.DROP
     });
 }
